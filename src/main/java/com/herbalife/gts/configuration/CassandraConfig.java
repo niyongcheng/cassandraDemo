63900821d9f992b2db65a16b8814cf6832a2a4fd
sid.ni <sid.ni@ef.com>
2021-02-17 09:54:01 +0800

    #commit initial codes

---
+++ src/main/java/com/herbalife/gts/configuration/CassandraConfig.java
@@ -0,0 +1,12 @@
+package com.herbalife.gts.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+
+@Configuration
+public class CassandraConfig extends AbstractCassandraConfiguration {
+    @Override
+    protected String getKeyspaceName() {
+        return "cycling";
+    }
+}
